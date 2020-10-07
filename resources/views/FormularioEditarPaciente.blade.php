1c849ec677d530e572a6d8978593d21400f4f646
Fredal Antonio Vasquez <antoniohilson21@gmail.com>
2020-10-07 14:16:29 -0600

    retoque de formulario editar paciente

--- resources/views/FormularioEditarPaciente.blade.php
+++ resources/views/FormularioEditarPaciente.blade.php
@@ -1,67 +1,101 @@
-
+@extends('Plantilla.Plantilla')
+@section('contenido')  
+<h3>formulario de edicion de datos del paciente</h3> 
+<div></div>    
+<div></div>    
                     <form method="post" action="{{route('paciente.update',['id'=> $pacientes-> id])}} ">
                       @csrf
+
                       <div class="form-group">
-                          <label for="nombres">Nombres:</label>
-                          <input type="text" class="form-control-file" name="nombres" id="nombres" placeholder="ingresar nombre del paciente"  value="{{ $pacientes->nombres }}" >
+                        <label for="nombres" class="col-sm-2 col-form-label col-form-label-lg" >Nombres:</label>
+                        <div class ="col-sm-6">
+                        <input type="text" class="form-control form-control-sm" name="nombres" id="nombres" placeholder="ingresar nombre del paciente"  value="{{ $pacientes->nombres }}" >
+                      </div>
+                    </div>
+
+              
+                      <div class="form-group">
+                          <label for="apellidos" class="col-sm-2 col-form-label col-form-label-lg" >Apellidos:</label>
+                          <div class="col-sm-6">
+                          <input type="text" class="form-control form-control-sm" name="apellidos" id="apellidos" placeholder="ingresar apellido del paciente"  value="{{ $pacientes->apellidos }}">
+                        </div>
                       </div>
 
                       <div class="form-group">
-                          <label for="apellidos">Apellidos:</label>
-                          <input type="text" class="form-control-file" name="apellidos" id="apellidos" placeholder="ingresar apellido del paciente"  value="{{ $pacientes->apellidos }}">
+                        <label for="identidad"class="col-sm-2 col-form-label col-form-label-lg">identidad:</label>
+                        <div class="col-sm-6">
+                        <input type="text" class="form-control form-control-sm" name="identidad" id="identidad" placeholder="ingresar identidad del paciente"  value="{{ $pacientes->identidad }}">
+                    </div>
+                  </div>
+
+                    <div class="form-group">
+                        <label for="identidad" class="col-sm-2 col-form-label col-form-label-lg">Sexo :</label>
+                        <div class="col-sm-6">
+                        <input type="text" class="form-control form-control-sm" name="sexo" id="sexo" placeholder="ingresar el sexo del paciente"  value="{{ $pacientes->sexo }}" >
                       </div>
-
-                      <div class="form-group">
-                        <label for="identidad">identidad:</label>
-                        <input type="text" class="form-control-file" name="identidad" id="identidad" placeholder="ingresar identidad del paciente"  value="{{ $pacientes->identidad }}">
-                    </div>
-                    <div class="form-group">
-                        <label for="identidad">Sexo :</label>
-                        <input type="text" class="form-control-file" name="sexo" id="sexo" placeholder="ingresar el sexo del paciente"  value="{{ $pacientes->sexo }}" >
                     </div>
 
                     <div class="form-group">
-                      <label for="fechaNacimiento">fecha de nacimiento:</label>
-                      <input type="text" class="form-control-file" name="fechaNacimiento" id="fechaNacimiento" placeholder="ingresar fecha de nacimiento del paciente"  value="{{ $pacientes->fechaNacimiento }}">
+                      <label for="fechaNacimiento" class="col-sm-2 col-form-label col-form-label-lg">fecha de nacimiento:</label>
+                     <div class="col-sm-6">
+                      <input type="text" class="form-control form-control-sm" name="fechaNacimiento" id="fechaNacimiento" placeholder="ingresar fecha de nacimiento del paciente"  value="{{ $pacientes->fechaNacimiento }}">
+                    </div>
+                    </div>
+
+                  <div class="form-group">
+                    <label for="departamento" class="col-sm-2 col-form-label col-form-label-lg">Departamento:</label>
+                    <div class="col-sm-6">
+                    <input type="text" class="form-control form-control-sm" name="departamento" id="departamento" placeholder="ingresar departamento del paciente"  value="{{ $pacientes->departamento }}">
+                  </div>
                   </div>
 
                   <div class="form-group">
-                    <label for="departamento">Departamento:</label>
-                    <input type="text" class="form-control-file" name="departamento" id="departamento" placeholder="ingresar departamento del paciente"  value="{{ $pacientes->departamento }}">
+                    <label for="ciudad" class="col-sm-2 col-form-label col-form-label-lg">ciudad:</label>
+                  <div class="col-sm-6">
+                    <input type="text" class="form-control form-control-sm" name="ciudad" id="ciudad" placeholder="ingresar ciudad del paciente"  value="{{ $pacientes->ciudad }}">
                   </div>
-
-                  <div class="form-group">
-                    <label for="ciudad">ciudad:</label>
-                    <input type="text" class="form-control-file" name="ciudad" id="ciudad" placeholder="ingresar ciudad del paciente"  value="{{ $pacientes->ciudad }}">
+                  </div>
                   
-                  </div><div class="form-group">
-                    <label for="direccion">Direccion:</label>
-                    <input type="text" class="form-control-file" name="direccion" id="direccion" placeholder="ingresar direccion del paciente"  value="{{ $pacientes->direccion }}">
+                  <div class="form-group">
+                    <label for="direccion" class="col-sm-2 col-form-label col-form-label-lg">Direccion:</label>
+                  <div class="col-sm-6">
+                    <input type="text" class="form-control form-control-sm" name="direccion" id="direccion" placeholder="ingresar direccion del paciente"  value="{{ $pacientes->direccion }}">
+                  </div>
                   </div>
                
                   <div class="form-group">
-                    <label for="telefonoFijo">Telefono fijo:</label>
-                    <input type="text" class="form-control-file" name="telefonoFijo" id="telefonoFijo" placeholder="ingresar telefono Fijo del paciente"  value="{{ $pacientes->telefonoFijo}}">
+                    <label for="telefonoFijo" class="col-sm-2 col-form-label col-form-label-lg">Telefono fijo:</label>
+                    <div class="col-sm-6">
+                    <input type="text" class="form-control form-control-sm" name="telefonoFijo" id="telefonoFijo" placeholder="ingresar telefono Fijo del paciente"  value="{{ $pacientes->telefonoFijo}}">
+                  </div>
                   </div>
 
                   <div class="form-group">
-                    <label for="telefonoCelular">Telefono celular:</label>
-                    <input type="text" class="form-control-file" name="telefonoCelular" id="telefonoCelular" placeholder="ingresar telefono Celular del paciente"  value="{{ $pacientes->telefonoCelular }}">
+                    <label for="telefonoCelular" class="col-sm-2 col-form-label col-form-label-lg">Telefono celular:</label>
+                  <div class="col-sm-6">
+                    <input type="text" class="form-control form-control-sm" name="telefonoCelular" id="telefonoCelular" placeholder="ingresar telefono Celular del paciente"  value="{{ $pacientes->telefonoCelular }}">
+                  </div>
                   </div>
 
                   <div class="form-group">
-                    <label for="profesion">Profesion:</label>
-                    <input type="text" class="form-control-file" name="profesion" id="profesion" placeholder="ingresar profesion del paciente"  value="{{ $pacientes->profesion }}">
+                    <label for="profesion" class="col-sm-2 col-form-label col-form-label-lg">Profesion:</label>
+                  <div class="col-sm-6">
+                    <input type="text" class="form-control form-control-sm" name="profesion" id="profesion" placeholder="ingresar profesion del paciente"  value="{{ $pacientes->profesion }}">
+                  </div>
                   </div>
 
                   <div class="form-group">
-                    <label for="empresa">Empresa:</label>
-                    <input type="text" class="form-control-file" name="empresa" id="empresa" placeholder="ingresar la empresa donde trabaja el paciente"  value="{{ $pacientes->empresa }}">
+                    <label for="empresa" class="col-sm-2 col-form-label col-form-label-lg">Empresa:</label>
+                  <div class="col-sm-6">
+                    <input type="text" class="form-control form-control-sm" name="empresa" id="empresa" placeholder="ingresar la empresa donde trabaja el paciente"  value="{{ $pacientes->empresa }}">
+                  </div>
                   </div>
 
                   <div class="form-group">
-                    <label for="observaciones">observaciones:</label>
-                    <input type="text" class="form-control-file" name="observaciones" id="observaciones" placeholder="Alguna observacion?"  value="{{ $pacientes->observaciones }}">
+                    <label for="observaciones" class="col-sm-2 col-form-label col-form-label-lg">observaciones:</label>
+                  <div class="col-sm-6">
+                    <input type="text" class="form-control form-control-sm" name="observaciones" id="observaciones" placeholder="Alguna observacion?"  value="{{ $pacientes->observaciones }}">
+                  </div>
                   </div>
 
                   <div class="modal-footer">
@@ -85,3 +119,4 @@
  
       
 </div>
+@endsection 
