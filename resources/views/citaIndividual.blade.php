93a459bb3853a344c291ed51000d2870fbe2b3b4
GeorginaMartinez <geopaomarmor1325@gmail.com>
2020-11-02 17:35:10 +0300

    citaindividuall

--- resources/views/citaIndividual.blade.php
+++ resources/views/citaIndividual.blade.php
@@ -114,7 +114,7 @@
         </td>
         <td>
         @forelse ($pacientes->citas as $tag) 
-         <p>{{ $tag->especialidad->Especialidad}}</p>
+         <p>{{ $tag->odontologo->especialidad}}</p>
          <hr>
         @empty
          vacio
