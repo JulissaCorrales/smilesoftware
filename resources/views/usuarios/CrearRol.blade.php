4af08c3e34bf100a03a50296676ebf465d69cc4a
GeorginaMartinez <geopaomarmor1325@gmail.com>
2020-11-27 14:08:42 +0300

    Instalacion del plugit de input tag Boostrap

--- resources/views/usuarios/CrearRol.blade.php
+++ resources/views/usuarios/CrearRol.blade.php
@@ -1,20 +1,25 @@
 @extends('Plantilla.Plantilla')
-@section('titulo','Editar Usuario')
+@section('titulo','Crear Rol')
 @section('contenido')
 <!DOCTYPE html>
 <html lang="en">
 <head>
 <style>
-#padre{
-    width:auto;
-    font:1em Tahoma;
-    margin: 5rem;
-    padding: 2rem;
-    border: 2px solid #ccc;
-}   #titulo{
+
+  #titulo{
     text-align:center;
 }
 </style>
+
+<link href="/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+<link href="/tagsinput/css/bootstrap-tagsinput.css" rel="stylesheet" type="text/css">
+<script src="/tagsinput/js/bootstrap-tagsinput.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.1/js/select2.min.js"></script>
+<link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.1/css/select2.min.css" rel="stylesheet" />
+<link href="https://cdnjs.cloudflare.com/ajax/libs/select2-bootstrap-css/1.4.6/select2-bootstrap.min.css" rel="stylesheet" />
+
+
 </head>
 <body>
     
@@ -48,6 +53,13 @@
         <label for="usuario">Slug</label>
         <input type="text" class="form-control-file" name="slug" id="slug" placeholder="Ingrese el slug">
     </div>
+
+    <div class="form-group">
+        <label for="permisos">Permisos</label>
+        <input type="text" value="" data-role="tagsinput" >
+    </div>
+
+    
     
 
    
@@ -69,6 +81,22 @@
     
     
     </div><!-- fin div padre -->
+
+@section('css_role')
+
+<link rel="stylesheet" href="/css/bootstrap-tagsinput.css">
+
+@endsection
+
+@section('js_role')
+<script src="/js/bootstrap-tagsinput.js"></script>
+@endsection
+
+
+
 </body>
+
+
+
 </html>
 @endsection
