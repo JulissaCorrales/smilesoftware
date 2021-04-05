c4feb83e8a1592f644565c1c4ab071bc46cb7461
GeorginaMartinez <geopaomarmor1325@gmail.com>
2021-04-05 09:22:55 +0300

    Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,7 @@
 
 <style>
 html,body{
-/*background-image: url('../assets/img/fondo34.jpg'); */
+background-image: url('../assets/img/fondo34.jpg');
 background-size: cover;
 background-repeat: no-repeat;
 height: 100%;
@@ -186,138 +186,81 @@ margin-left: 4px;
 <html>
     
 <head>
-<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-<!--===============================================================================================-->	
-	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="fonts/iconic/css/material-design-iconic-font.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
-<!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
-<!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="css/util.css">
-	<link rel="stylesheet" type="text/css" href="css/main.css">
-<!--===============================================================================================-->
+	<title>My Awesome Login Page</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
 </head>
-<body style="background-color: #999999;">
-	
-	<div class="limiter">
-		<div class="container-login100" style="position:absolute; top:-3px;">
-			<div class="" style="background-image: url('../assets/img/fondo21.jpg'); background-repeat: no-repeat;
-  background-position: center;
-  background-size: cover;
-  width: calc(100% - 520px);
-  z-index: 1;">   position: relative;
+<!--Coded with love by Mutiullah Samim-->
+<body>
+	<div class="container h-100">
+		<div class="d-flex justify-content-center h-100">
+			<div class="user_card">
+				<div class="d-flex justify-content-center">
+					<div class="brand_logo_container">
+						<img src="{{ asset('Imagenes/foto1.jpg') }}" class="brand_logo" alt="Logo">
+					</div>
+					@if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
 
+				</div>
+				<div class="d-flex justify-content-center form_container">
+				<form method="POST" action="{{ route('login') }}">
+              @csrf
+					<div class="input-group form-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"><i class="fas fa-user"></i></span>
+						</div>
+            <input type="name" id="name" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" placeholder="Nombre de usuario" required autocomplete="name" autofocus>
+            @error('name')
+                      <span class="invalid-feedback" role="alert">
+                          <strong>{{ $message }}</strong>
+                      </span>
+                  @enderror
+					</div>
+            <div class="input-group form-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"><i class="fas fa-key"></i></span>
+						</div>
+            <input type="password" id="inputPassword" class="form-control @error('password') is-invalid @enderror" placeholder="Ingrese su contraseña" name="password" required autocomplete="current-password">
+            @error('password')
+                      <span class="invalid-feedback" role="alert">
+                          <strong>{{ $message }}</strong>
+                      </span>
+                  @enderror
+					</div>
 
-
-<?php $logotipos=App\Logotipo::where('id','=',1)->get();?>
-                        @forelse($logotipos  as $tag)
-                    <img  style="  border-radius: 70%; position:absolute; left: 400px;"class="logo" id="imlogoactual"src="{{Storage::url($tag->logo)}}" class="mr-3" alt="image" width="80px" high="100px" id="dos">
-                    @empty
-                    <img  style="  border-radius: 90%;  position:absolute; left: 400px; top: 230px;" class="logo" src="{{ asset('Imagenes/dental2.jpg') }}" class="mr-3"  width="100px"> 
-                    @endforelse
-            
-<div style=" position:absolute; top: 250px; left:250px;">
-				<h3 style="color: #d6f5f5; position:absolute; 
-                     text-shadow: 4px 0 #ffb31a, 0 4px #ffb31a, 4px 0 #ffb31a, 0 4px #ffb31a;font-family: Cursive ;left: 80px; top: -120px;font-size: 80px;">Smile</h3>
-				<h3 style="color: #d6f5f5; position:absolute;
-                     text-shadow: 4px 0 #ffb31a, 0 4px #ffb31a, 4px 0 #ffb31a, 0 4px #ffb31a;font-family: cursive ;left: 30px; top: 70px;font-size: 70px;">Software</h3>
-
-	</div>
-
- 
-  </div>
-
-  
-
-
-			<div class="wrap-login100 p-l-50 p-r-50 p-t-72 p-b-50" style=" background-image: linear-gradient(to bottom, #ffeecc ,#e6ffff );">
-				<form class="login100-form validate-form" method="POST" action="{{ route('login') }}">
-				@csrf
-					<span class="login100-form-title p-b-59" style="text-shadow: 2px 0 #ffb31a, 0 2px #ffb31a, 2px 0 #ffb31a, 0 2px #ffb31a;">
-						Iniciar Sesion 
-					</span>
-
-
-					
-					
-
-					<div class="wrap-input100 validate-input" data-validate="Name is required">
 						
-						<input class="input100" type="text" name="name" placeholder="Nombre del Usuario">
-						<span class="focus-input100"></span>
-					</div>
-
-					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
-					
-						<input type="password" id="inputPassword" class="input100 @error('password') is-invalid @enderror" placeholder="Ingrese su contraseña" name="password" required autocomplete="current-password">
-						<span class="focus-input100"></span>
-					</div>
-
-
-					<div class="flex-m w-full p-b-33">
-					<div class="form-group">
-			  <div class="form-check" style="position:absolute; top: 340px; left:50px;">
+						
+              <div class="form-group">
+			  <div class="form-check">
                                     <input  type="checkbox" name="remember" class="form-check-input ios-switch-control-input" value="1" id="remember">
 
-                                    <label class="form-check-label" for="remember" style="font-family: FontAwesome;">
+                                    <label class="form-check-label" for="remember">
                                         {{ __('Remember Me') }}
-                                    </label>						
-	</div>
-
-	<div class="mt-4">
+                                    </label>
+                                </div>
+          <button class="btn btn-lg btn-primary " type="submit" id="butr">Entrar</button>
+					
+					
+				   </div>
+					</form>
+				</div>
+		
+				<div class="mt-4">
 				
-				<div class="container" >
-			  @if (Route::has('password.request'))
-				  <a class="small" href="{{ route('password.request') }}" style="position:absolute; top: 340px; left:250px; font-family: FontAwesome;
-  font-size: 13px;">¿Olvidaste tu contraseña?</a></div>
-			  @endif
-		</div>
-					</div>
-
-	
-			<button class="btn btn-lg btn-primary " type="submit" id="butr" style="position:absolute; width: 350px; top: 380px; left:90px;">Entrar
-	</button>		
-			<img class="logo" src="assets/img/muela.png" class="mr-3" id="logo1" style="position:absolute;  left: 160px; top: 420px; width: 350px;
-  height: 220px; ">  
-		</div>
-				</form>
+					<div class="text-center">
+                  @if (Route::has('password.request'))
+                      <a class="small" href="{{ route('password.request') }}">'¿Olvidaste tu contraseña?</a></div>
+                  @endif
+			</div>
+				</div>
 			</div>
 		</div>
 	</div>
-	
-<!--===============================================================================================-->
-	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
-<!--===============================================================================================-->
-	<script src="vendor/animsition/js/animsition.min.js"></script>
-<!--===============================================================================================-->
-	<script src="vendor/bootstrap/js/popper.js"></script>
-	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
-<!--===============================================================================================-->
-	<script src="vendor/select2/select2.min.js"></script>
-<!--===============================================================================================-->
-	<script src="vendor/daterangepicker/moment.min.js"></script>
-	<script src="vendor/daterangepicker/daterangepicker.js"></script>
-<!--===============================================================================================-->
-	<script src="vendor/countdowntime/countdowntime.js"></script>
-<!--===============================================================================================-->
-	<script src="js/main.js"></script>
-
 </body>
 </html>
 
