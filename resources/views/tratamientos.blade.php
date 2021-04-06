9a08c4f799debd3703a23e33fce77475ad52a9a5
JulissaCorrales <julissa.corrales@unah.hn>
2021-04-06 10:03:20 -0600

    convertir vista de editar tratamiento en modal

--- resources/views/tratamientos.blade.php
+++ resources/views/tratamientos.blade.php
@@ -71,16 +71,68 @@
      <td>
      @can('update',$tratamiento)
      
-       <a class="btn btn-warning " href="{{route('tratamiento.editar',['id'=>$tratamiento->id])}}">
+       <button type="button" class="btn btn-success" data-toggle="modal" data-target="#exampleModalLong-{{$tratamiento->id}}" >
             <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-pencil-square" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
             <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
             <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
             </svg>
-          </a>
+          </button>
       @endcan
+    <!-- modal editar -->
+          <!-- Modal -->
+          <div class="modal fade" id="exampleModalLong-{{$tratamiento->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+          <div class="modal-dialog" role="document">
+          <div class="modal-content">
+          <div class="modal-header" style="background-color:#212121;color:white">
+          <h5 class="modal-title"  id="exampleModalLongTitle">
+           <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
+            <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z"/>
+            </svg>
+
+          Editar Tratamiento
+               
+          </h5>
+          <button type="button"  style="color:white"class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+          </button>
+          </div>
+          <div class="modal-body">
+          <form method="post" action="{{route('tratamiento.update',['id'=> $tratamiento->id])}} ">
+
+          @csrf
+          @method('put')
+          <!-- Categoria-->
+
+          <div class="form-group" id="divcate">
+          <label for="categoria" class="control-label">Nombre del Tratamiento:</label>
+          <input  style="background-color:#FFFDE7" type="text"  class="form-control-file" placeholder="Ingrese la categoria del gasto" name="categoria" id="categoria  "   value="{{ $tratamiento->categoria }}"> 
+          </div>
+
+          <!-- Tipo-->
+          <div class="form-group" id="div2">
+          <label for="detalle" class="control-label">Tipo:</label>
+          <input type="text"  style="background-color:#FFFDE7"  class="form-control-file" name="tipo" id="tipo" placeholder="Ingrese el tipo de gasto" value="{{ $tratamiento->tipo }}">
+          </div>
+
+          <div class="form-group" id="div6" align="center">
+          <input type="reset" class="btn btn-danger">
+          <button id="botonContinuar"type="submit"class="btn btn-primary" data-toggle="modal" >
+          Actualizar
+          </button>
+
+          </div>
+
+          </form>
+          </div>
+          </div>
+          </div>
+          </div>
+
+          
+      <!-- fin modal editar -->
 
     @can('delete',$tratamiento)
-     <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#modal-{{$tratamiento->id}}"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+     <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#modall-{{$tratamiento->id}}"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
      <path fill-rule="evenodd" d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5a.5.5 0 0 0-1 0v7a.5.5 0 0 0 1 0v-7z"/>
      </svg>
       
@@ -92,7 +144,7 @@
   
      </td>
 <!-- Modal -->
-<div class="modal fade" id="modal-{{$tratamiento->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="modall-{{$tratamiento->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
       <div class="modal-dialog" role="document">
           <div class="modal-content">
               <div class="modal-header">
