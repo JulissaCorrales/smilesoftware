16ae6b94472e08c2eecdce6e2cdc510d2947a951
GeorginaMartinezMorgan23 <smilesoftware2021@gmail.com>
2021-04-12 08:45:53 +0300

    gasto

--- resources/views/gastos.blade.php
+++ resources/views/gastos.blade.php
@@ -116,7 +116,7 @@ color:white;
             <td>{{$gasto->fechapago}}</td>
             <td>{{$gasto->monto}}</td>
             @can('update',$gasto)
-            <td >  <a class="btn btn-warning "  data-toggle="modal" data-target="#modal-{{$gasto->id}}">
+            <td >  <a class="btn btn-warning "  data-toggle="modal" data-target="#modall-{{$gasto->id}}">
             <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-pencil-square" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
             <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
             <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
@@ -124,10 +124,10 @@ color:white;
             </a>
 
              
- <div class="modal fade" id="modal-{{$gasto->id}}" >
+ <div class="modal fade" id="modall-{{$gasto->id}}" >
             <div class="modal-dialog" role="document">
                 <div class="modal-content" >
-                <div id="div1"class="modal-header" style="background-color:#293d3d; color:white;  height:100px;">
+                <div id="div1"class="modal-header" style="background-color:#293d3d; color:white;  height:80px;">
                 
   
                     <h4  class="modal-title" id="myModalLabel">  <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-pencil-square" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
@@ -225,7 +225,7 @@ color:white;
                 <div class="modal fade" id="modal-{{$gasto->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                     <div class="modal-dialog" role="document" style="position:absolute; top: 100px; left:500px;">
                         <div class="modal-content">
-                            <div class="modal-header" style=" background-color:#293d3d; color:white;  height:100px;">
+                            <div class="modal-header" style=" background-color:#293d3d; color:white;  height:80px;">
                                 <h5 class="modal-title" id="exampleModalLabel">  <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                 <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
                 <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
