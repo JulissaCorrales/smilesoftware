466bfd500b2f7f6b6f72b2c60199ed30be9a9785
Fredal Antonio Vasquez <antoniohilson21@gmail.com>
2021-04-23 11:18:19 -0600

    adicion de ventana modal de editar medio de pago en la funcion de medios de pago esto con la intencion de descargar las vistas y aligerar la app

--- resources/views/mediopagos.blade.php
+++ resources/views/mediopagos.blade.php
@@ -148,12 +148,65 @@ background-color: #57ECC7
            
            <td>
            @can('update',$mediopago)
-           <a class="btn btn-warning " href="{{route('mediopago.editar',['id'=>$mediopago->id])}}">
-            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-pencil-square" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+           
+
+            <button type="button" class="btn btn-success" data-toggle="modal" data-target="#exampleModalLong-{{$mediopago->id}}" >
+            Editar Medio de Pago<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-pencil-square" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
             <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
             <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
             </svg>
-            Editar</a>
+          </button>
+
+            <!-- modal editar -->
+          <!-- Modal -->
+          <div class="modal fade" id="exampleModalLong-{{$mediopago->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+          <div class="modal-dialog modal-dialog-centered" role="document">
+          <div class="modal-content">
+          <div class="modal-header" style="background-color:#26A69A;color:white">
+          <h5 class="modal-title"  id="exampleModalLongTitle">
+           <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
+            <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001zm-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z"/>
+            </svg>
+
+          Editar Medio de Pago
+               
+          </h5>
+          <button type="button"  style="color:white"class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+          </button>
+          </div>
+          <div class="modal-body">
+
+
+          
+          <form method="post" action="{{route('mediopago.update',['id'=> $mediopago-> id])}} ">
+
+    @csrf
+    @method('put')
+     <!-- Producto-->
+                
+     <div class="form-group" id="divcate">
+                    <label for="nombre" class="control-label">Nombre del Medio de Pago:</label>
+                    <input type="text"  class="form-control-file" placeholder="Ingrese nombre del inventario" name="nombre" id="nombre"   value="{{$mediopago->nombre}}"> 
+                    </div>
+                    
+                    <div class="form-group" id="div6">
+                    <button style="background-color:purple"type="button" onclick="location.href='/pantallainicio/mediopago'"class="btn btn-secondary" data-dismiss="modal">Atrás</button>
+                    <input type="reset" class="btn btn-danger">
+                    <button id="botonContinuar"type="submit"class="btn btn-primary" data-toggle="modal" >
+                        Actualizar
+                    </button>
+                            
+                    </div> 
+    </form>
+
+
+
+          </div>
+          </div>
+          </div>
+          </div>
+
             @else
             No tiene el permiso
             @endcan
