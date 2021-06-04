b10c41328b6e3d4aa72069ce0fe7855a240bc3ef
GeorginaMartinezMorgan23 <smilesoftware2021@gmail.com>
2021-06-04 16:51:41 +0300

    Vista Administracion

--- resources/views/administracion.blade.php
+++ resources/views/administracion.blade.php
@@ -5,125 +5,231 @@
 
 @section('content')
 
+<style>
+.container {
+  position:relative;
+ width:900px;
+
+}
+
+.image {
+  opacity: 1;
+  display: block;
+  width: 50px;
+  height: 50px;
+  transition: .5s ease;
+  backface-visibility: hidden;
+border-style: solid;
+  border-color: #1687a7;
+}
+
+.middle {
+  transition: .5s ease;
+  opacity: 0;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%)
+}
+
+.container:hover .image {
+  opacity: 0.3;
+}
+
+.container:hover .middle {
+  opacity: 1;
+}
+
+
+</style>
+
+
  <div class="card mb-3">
           <div class="card-header">
-           <h4><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-person-badge-fill" viewBox="0 0 16 16">
-  <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2zm4.5 0a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1h-3zM8 11a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm5 2.755C12.146 12.825 10.623 12 8 12s-4.146.826-5 1.755V14a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-.245z"/>
+           <h4 style="color:#1687a7"> <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-file-earmark-text" viewBox="0 0 16 16">
+  <path d="M5.5 7a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5zM5 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm0 2a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5z"/>
+  <path d="M9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.5L9.5 0zm0 1v2A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5z"/>
 </svg>Administración</h4>
- <p>En esta Sección muestra las opciones:Odontologos,Inventarios,Gastos,Logotipo,Tratamiento,Medios de Pagos, Laboratorios para que el usuario pueda interactuar en la Sección que desee</p>
+ <p>En esta Sección muestra las opciones:Odontólogos ,Inventarios,Gastos,Logotipo,Tratamiento,Medios de Pagos, Laboratorios para que el usuario pueda interactuar en la Sección que desee.</p>
 
 
             </div>
           <div class="card-body">
 
+<div class="container">
+  <img src="{{ asset('Imagenes/Doctor1.png')}}" alt="Avatar" class="image" style="width:170px; height:170px; position:absolute; left:-160px;">
+
+  <div class="middle">
+    <div class="text">
+
 
  @canany(['isAdmin','isSecretaria'])
-              <a class="btn btn-outline-info" href="/pantallainicio/odontologo"  >
-               <img class="logo" style="" src="{{ asset('Imagenes/Doctor1.jpeg')}}" class="mr-3" id="logo1" width="100px;" height="100px;"> 
-                
-              <br>
- <span>Odontólogos </span> 
+              <a class="btn btn-outline-info" href="/pantallainicio/odontologo"  style=" background-color:#d3e0ea; color:#276678; position:absolute; left:-610px; top:70px; width:169px; height:40px;" >
+            Odontólogos 
               </a>
             @endcanany
 
+   </div>
+  </div>
+</div>
+  
+
+
+<div class="container">
+  <img src="{{ asset('Imagenes/inventario.jpeg')}}" alt="Avatar" class="image" style="width:170px; height:170px;">
+  <div class="middle">
+    <div class="text">
 
 
    @can('view',App\Inventario::class)
-            <a class="btn btn-outline-info" href="/inventario/">
-              <img class="logo" style="" src="{{ asset('Imagenes/inventario.jpeg')}}" class="mr-3" id="logo1" width="100px;" height="100px;"> 
-             
-    <br>
-
- <span>Inventarios</span>
+            <a class="btn btn-outline-info" href="/inventario/" style=" background-color:#d3e0ea; color:#276678; position:absolute; left:-435px; top:-10px; width:169px; height:40px;">
+              
+ Inventarios
            </a>
            @endcan
 
+   </div>
+  </div>
+</div>
+
+
+
+
+<div class="container">
+  <img src="{{ asset('Imagenes/gastos.jpeg')}}" alt="Avatar" class="image" style="width:170px; height:170px; position:absolute; top:-170px; left:190px;">
+  <div class="middle">
+    <div class="text">
 
 
 
             @can('view',App\Gasto::class)
-              <a class="btn btn-outline-info" href="/pantallainicio/gastos">
-                <img class="logo" style=" " src="{{ asset('Imagenes/dental2.jpg') }}" class="mr-3" id="logo1" width="100px;" height="100px;"> 
-               <br>
-               <span>Gastos</span> 
+              <a class="btn btn-outline-info" href="/pantallainicio/gastos" style=" background-color:#d3e0ea; color:#276678; position:absolute; left:-260px; top:-100px; width:169px; height:40px;">
+               
+               Gastos
               </a>
             @endcan
+
+   </div>
+  </div>
+</div>
+ 
+
+
+
+
+
+<div class="container">
+  <img src="{{ asset('Imagenes/logotipo.jpeg')}}" alt="Avatar" class="image" style="width:170px; height:170px; position:absolute; top:-170px; left:366px;">
+  <div class="middle">
+    <div class="text">
 
 
             @can('view',App\Logotipo::class)
-              <a class="btn btn-outline-info" href="{{route('logotipo.ver')}}">
-                <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-card-image" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd" d="M14.5 3h-13a.5.5 0 0 0-.5.5v9c0 .013 0 .027.002.04V12l2.646-2.354a.5.5 0 0 1 .63-.062l2.66 1.773 3.71-3.71a.5.5 0 0 1 .577-.094L15 9.499V3.5a.5.5 0 0 0-.5-.5zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13zm4.502 3.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
-                </svg>
-<br>
-<span>Logotipo</span>
+              <a class="btn btn-outline-info" href="{{route('logotipo.ver')}}" style=" background-color:#d3e0ea; color:#276678; position:absolute; left:-84px; top:-100px; width:169px; height:40px;">
+  
+               
+
+Logotipo
              
               </a>
             @endcan
-            @canany(['isAdmin','isSecretaria'])
-            <a class="btn btn-outline-info"href="/tratamiento/">
-              <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-alarm" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-              <path fill-rule="evenodd" d="M6.5 0a.5.5 0 0 0 0 1H7v1.07a7.001 7.001 0 0 0-3.273 12.474l-.602.602a.5.5 0 0 0 .707.708l.746-.746A6.97 6.97 0 0 0 8 16a6.97 6.97 0 0 0 3.422-.892l.746.746a.5.5 0 0 0 .707-.708l-.601-.602A7.001 7.001 0 0 0 9 2.07V1h.5a.5.5 0 0 0 0-1h-3zm1.038 3.018a6.093 6.093 0 0 1 .924 0 6 6 0 1 1-.924 0zM8.5 5.5a.5.5 0 0 0-1 0v3.362l-1.429 2.38a.5.5 0 1 0 .858.515l1.5-2.5A.5.5 0 0 0 8.5 9V5.5zM0 3.5c0 .753.333 1.429.86 1.887A8.035 8.035 0 0 1 4.387 1.86 2.5 2.5 0 0 0 0 3.5zM13.5 1c-.753 0-1.429.333-1.887.86a8.035 8.035 0 0 1 3.527 3.527A2.5 2.5 0 0 0 13.5 1z"/>
-              </svg>
-<br>
-             <span> Tratamientos</span>
-            </a>
-            @endcanany
-           
-           @canany(['isAdmin','isSecretaria'])
-            <a class="btn btn-outline-info" href="/pantallainicio/mediopago">
-              <svg width="1em" height="2em" viewBox="0 0 16 16" class="bi bi-cash" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-              <path fill-rule="evenodd" d="M15 4H1v8h14V4zM1 3a1 1 0 0 0-1 1v8a1 1 0 0 0 1   1h14a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1H1z"/>
-                <path d="M13 4a2 2 0 0 0 2 2V4h-2zM3 4a2 2 0 0 1-2 2V4h2zm10 8a2 2 0 0 1 2-2v2h-2zM3 12a2 2 0 0 0-2-2v2h2zm7-4a2 2 0 1 1-4 0 2 2 0 0 1 4 0z"/>
-              </svg>
-<br>
-             <span> Medios de Pagos </span>
-            </a>
-            @endcanany
-            
-            <a class="btn btn-outline-info" href="/pantallainicio/laboratorios">
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-wallet-fill" viewBox="0 0 16 16">
-                <path d="M1.5 2A1.5 1.5 0 0 0 0 3.5v2h6a.5.5 0 0 1 .5.5c0 .253.08.644.306.958.207.288.557.542 1.194.542.637 0 .987-.254 1.194-.542.226-.314.306-.705.306-.958a.5.5 0 0 1 .5-.5h6v-2A1.5 1.5 0 0 0 14.5 2h-13z"/>
-                <path d="M16 6.5h-5.551a2.678 2.678 0 0 1-.443 1.042C9.613 8.088 8.963 8.5 8 8.5c-.963 0-1.613-.412-2.006-.958A2.679 2.679 0 0 1 5.551 6.5H0v6A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-6z"/>
-              </svg>
-              <br>
-              <span>Laboratorios</span>
-            </a>
-
-            
-
+   </div>
   </div>
-  <!-- /#wrapper -->
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            <div class="container">
-
-
-           
 </div>
+
+ 
+<div class="container">
+  <img src="{{ asset('Imagenes/tratamiento.jpeg')}}" alt="Avatar" class="image" style="width:170px; height:170px; position:absolute; top:-170px; left:543px;">
+  <div class="middle">
+    <div class="text">
+
+
+            @canany(['isAdmin','isSecretaria'])
+            <a class="btn btn-outline-info"href="/tratamiento/"  style=" background-color:#d3e0ea; color:#276678; position:absolute; left:95px; top:-100px; width:169px; height:40px;" >
+               
+ Tratamientos
+            </a>
+            @endcanany
+   </div>
+  </div>
+</div>
+ 
+
+
+
+ 
+<div class="container">
+  <img src="{{ asset('Imagenes/pagos.jpeg')}}" alt="Avatar" class="image" style="width:170px; height:170px; position:absolute; top:-170px; left:720px;">
+  <div class="middle">
+    <div class="text">
+
+           @canany(['isAdmin','isSecretaria'])
+            <a class="btn btn-outline-info" href="/pantallainicio/mediopago" style=" background-color:#d3e0ea; color:#276678; position:absolute; left:270px; top:-100px; width:169px; height:40px;" >
+            
+ Medios de Pagos 
+            </a>
+            @endcanany
+
+            
+   </div>
+  </div>
+</div>
+ 
+
+
+
+ 
+<div class="container">
+  <img src="{{ asset('Imagenes/laboratorios.jpeg')}}" alt="Avatar" class="image" style="width:170px; height:170px; position:absolute; top:-170px; left:900px;">
+  <div class="middle">
+    <div class="text">
+
+         
+            <a class="btn btn-outline-info" href="/pantallainicio/laboratorios" style=" background-color:#d3e0ea; color:#276678; position:absolute; left:450px; top:-100px; width:169px; height:40px;">
+            
+              Laboratorios
+            </a>
+
+            
+   </div>
+  </div>
+</div>
+ 
+
+<p style="position:relative;"></p>
+
+<p style="position:absolute; left:50px; top:330px; color:#1687a7"> <b>
+  Odontólogos </b> </p>
+
+<p style="position:absolute; left:230px; top:330px; color:#1687a7"> <b> Inventarios </b></p>
+
+<p style="position:absolute; left:430px; top:330px; color:#1687a7"><b>Gastos</b></p>
+
+<p style="position:absolute; left:590px; top:330px; color:#1687a7"><b>Logotipos</b></p>
+
+<p style="position:absolute; left:760px; top:330px; color:#1687a7"><b>Tratamientos</b></p>
+
+<p style="position:absolute; left:930px; top:330px; color:#1687a7"><b>Medios Pagos</b></p>
+
+<p style="position:absolute; left:1120px; top:330px; color:#1687a7"><b>Laboratorios</b></p>
+
+
+<h1></h1>
+
+
+
+
+
+
+
+
+
+
+
+
+
+          </div>
         
 
 @endsection
