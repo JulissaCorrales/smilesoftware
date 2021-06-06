235ac14931017ae5d1bf7b5622e22267d5669a61
JulissaCorrales <julissa.corrales@unah.hn>
2021-06-06 15:14:48 -0600

    correccion ruta nuevo usuario

--- resources/views/usuarios/VistaUsuarios.blade.php
+++ resources/views/usuarios/VistaUsuarios.blade.php
@@ -22,19 +22,16 @@
             </svg>Usuario</h4>
             <p>En esta Sección se muestra los Usuario registrados y también se podra editar datos, crear un nuevo Usuario, borrar el Usuario registrado, ver la especialidad del Usuario y la Identidad del mismo</p>
             
-            @can('$nuevo', App\Usuario::class)<!-- boton de nuevo usuario -->
+          <!-- boton de nuevo usuario -->
             <div>
-                <a type="button"style="background-color:#28a4a4; color:#c1f0f0; position: absolute;
-                            left: 100px;
-                            top:  85px;"  class="btn" href="/pantallainicio/{{$usuario->id}}/editar"><svg width="25" height="25" viewBox="0 0 16 16" class="bi bi-cloud-download" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <a type="button"style="background-color:#28a4a4; color:#c1f0f0; position: relative;border: 2px solid #276678;"  class="btn" href="{{route('usuario.nuevo')}}"><svg width="25" height="25" viewBox="0 0 16 16" class="bi bi-cloud-download" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                             <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
                             <path fill-rule="evenodd" d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
                             </svg> 
                                 Nuevo Usuario
                             </a>
                         </div>
-            @endcan 
-
+      
             </div>
         </div>
 
