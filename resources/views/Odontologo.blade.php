c5f15640eba15f810b933e2712a6cd5349e44912
GeorginaMartinezMorgan23 <smilesoftware2021@gmail.com>
2021-06-11 20:48:49 +0300

    Vista Odontologo completa

--- resources/views/Odontologo.blade.php
+++ resources/views/Odontologo.blade.php
@@ -157,7 +157,7 @@
                   <!--<span aria-hidden="true">&times;</span>-->
                   </button>
               </div>
-              <div class="modal-body">
+              <div class="modal-body" style="color:black;">
                   ¿Desea realmente eliminar el Odontologo {{$odontologo->nombres}} {{$odontologo->apellidos}}?
               </div>
               <div class="modal-footer">
@@ -187,11 +187,14 @@
             
 
 				<h4  class="modal-title" id="modal" >
-        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-pencil-square" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+        <!--<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-pencil-square" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
         <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
         <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
-        </svg>
-        Editar Odontologo</h4>
+        </svg> -->
+<img style=" border-radius: 50%; " src='/Imagenes/{{$odontologo->imagen}}' width=" 70px" height="70px"  >
+        Editar Odontologo </h4>  
+ <p style="margin-top:50px; margin-left:-180px;">{{$odontologo->nombres}} {{$odontologo->apellidos}} </p>
+         
      
         	<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 					<span aria-hidden="true">&times;</span>
@@ -201,7 +204,7 @@
 			<div class="modal-body" id="bodymodal">
       <div class="row"> 
         <div id="imagen4" class="col-md-12">
-          <img style=" border-radius: 50%;" src='/Imagenes/{{$odontologo->imagen}}' width=" 70px" height="70px"id="datos6" >
+          
           </div>
       </div>
       <hr style="color:#1687a7">
@@ -298,17 +301,10 @@
       ?>
       <div class="col-md-4">
         <label for="state_id">Especialidad:</label>
-        <select required name="especialidad" class="form-control">
-          <option value="" selected>Actual:</option>
-            <?php
-            $getDoctor =$mysqli->query("select * from especialidads order by id");
-            while($f=$getDoctor->fetch_object()) {
-            echo $f->id;
-            echo $f->Especialidad;
-            ?>
-          <option value="<?php echo $f->id; ?>"><?php echo $f->Especialidad ?></option>
-          <?php }?>     
-        </select>
+       <input type="text" data-role="tagsinput"  class="form-control" id="roles_permisos" name="especialidades" value="@foreach ($odontologo->especialidades as $permiso)
+            {{$permiso->Especialidad. ','}}
+        @endforeach" 
+        >   
       </div>
     </div>
     <div class="row">
