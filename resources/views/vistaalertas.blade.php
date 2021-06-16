38b60974b5d8ebcba234b708b82610f91bd7cb4a
JulissaCorrales <julissa.corrales@unah.hn>
2021-06-15 22:08:43 -0600

    Cambio de vista alertas y evolucion

--- resources/views/vistaalertas.blade.php
+++ resources/views/vistaalertas.blade.php
@@ -6,70 +6,56 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script><!-- para que funcione boton desplegable -->
-    <style>
-    #padre{
-        width: auto;
-        margin-left: 400px; 
-        margin-top: 6em; 
-        margin-right: 4em; 
-        height: auto;
-        border: 5px solid gray;
-    }
-    #areadetexto{
-        color:red;
-    }
-    table, th, td {
-    border: 1px solid #e7b3ff;
-    border-collapse: collapse;
-}
-
-    th{
-        background-color:#32cdcd  ;
-        font-size:18px; font-family: Times New Roman, Times, serif; color:#293d3d;
-    }
-    #divtabla{
-        text-align:center;
-          margin-left: 4em; 
-    }
-        #padre2{
-    height: auto;
-    position: relative;
-    width: 800px;
-    margin-left: 400px; 
-    margin-top: 6em; 
-    margin-right: 4em; 
-    
-    }
-    h4{text-align:center;
-    margin:2em;
-    }
-</style>
 </head>
-<body>
-    <div id="padre" class="container">
-    <h3>Alertas</h3>
+<body id="page-top">
+    <div id="padre" class="card">
+    
+<div>
+<div>@if(session('mensaje'))
+    <div class="alert alert-success">
+        {{session('mensaje')}}
+          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+    </div>
+@endif
 @if ($errors->any())
-<div class="alert alert-danger">
-<ul>
-@foreach ($errors->all() as $error)
-<li>{{ $error }}</li>
-@endforeach
-</ul>
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}
+                     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                          <span aria-hidden="true">&times;</span>
+                      </button>
+                </li>
+            @endforeach
+         
+        </ul>
+        
+    </div>
+@endif</div>
 </div>
-@endif
-@if(session('mensaje'))
-<div class="alert alert-success">
-{{session('mensaje')}}
-</div>
-@endif
-<!-- Formulario para crearle una alerta al paciente en especifico -->
+
+  
+    
+<div class="card-header">
+        <h4 class="card-title">
+        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-exclamation-triangle" viewBox="0 0 16 16">
+        <path d="M7.938 2.016A.13.13 0 0 1 8.002 2a.13.13 0 0 1 .063.016.146.146 0 0 1 .054.057l6.857 11.667c.036.06.035.124.002.183a.163.163 0 0 1-.054.06.116.116 0 0 1-.066.017H1.146a.115.115 0 0 1-.066-.017.163.163 0 0 1-.054-.06.176.176 0 0 1 .002-.183L7.884 2.073a.147.147 0 0 1 .054-.057zm1.044-.45a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566z"/>
+        <path d="M7.002 12a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 5.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995z"/>
+        </svg>
+        Alertas</h4>
+        <p lass="card-text">En esta sección se muestran las alertas a tener en cuenta de cada paciente, también se podrá crear de manera general para su después utilización. </p>
+    </div>
+    <div class="card-body">
+        <!-- Formulario para crearle una alerta al paciente en especifico -->
         <form method="POST" action="">
         
             @csrf
-            <textarea required id="areadetexto" name="alerta" value="text" rows="4" cols="100" placeholder="Escriba la alerta a tener en cuenta del paciente" ></textarea>
+            <textarea  id="areadetexto" name="alerta" value="text" rows="4" cols="100" placeholder="Escriba la alerta a tener en cuenta del paciente" ></textarea>
             <br>
             <?php
             $mysqli= new mysqli ('127.0.0.1','root','','smilesoftware');
@@ -84,21 +70,20 @@
                 <?php
             } 
             ?>
-            <hr>
-            <div>
+            
+            <div class="modal-footer"> 
             @can('isAdmin')
             <button type="button" id="config"class="btn btn-outline-info" data-toggle="modal" data-target="#configurar">Configurar alertas predeterminadas </button>
             @endcan
-            <button type="submit" class="btn btn-primary" id="guardar" >Guardar </button>
+            <button type="submit" class="btn" style="background-color:#276678;color:white;" id="guardar" >Guardar </button>
             </div>
             </form>
     
-    
-    </div>
-    <div id="padre2" class="container">
-    <h4>Alertas Actuales del Paciente</h4>
-    <div id="divtabla">
-    <table class="table" id="datatable">
+<hr>
+    <div id="padre2" style="text-align:center">
+    <h4 class="card-title" id="titulo2" >Alertas Actuales del Paciente</h4>
+    <div id="divtabla" class="table-responsive">
+    <table class="table table-bordered" width="100%" cellspacing="0" id="datatable">
     <thead><tr><th>Alerta</th><th>Acción</th></tr></thead>
     <tbody>
     @forelse($pacientes->alertas as $ver)
@@ -118,12 +103,12 @@
         <button   class="btn btn-danger" data-toggle="modal" data-target="#modal-{{$ver->id}}"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
         <path fill-rule="evenodd" d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5a.5.5 0 0 0-1 0v7a.5.5 0 0 0 1 0v-7z"/>
         </svg>
-        Eliminar
+        <!-- Eliminar -->
         </button>
         <div class="modal fade" id="modal-{{$ver->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-        <div class="modal-content" style="position:absolute; left:50px; top:100px;">
-        <div class="modal-header" style="background-color:#293d3d; color:white;  height:80px; ">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+        <div class="modal-header" style="background-color:#276678; color:white;  height:80px; ">
         <h5 class="modal-title" id="exampleModalLabel"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
         <path fill-rule="evenodd" d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5a.5.5 0 0 0-1 0v7a.5.5 0 0 0 1 0v-7z"/>
         </svg> Eliminar Alerta del paciente</h5>
@@ -155,7 +140,8 @@
             <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
             <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
             </svg>
-            Editar</a>
+            <!-- Editar -->
+            </a>
     </td>
     @endcan
     </tr>
@@ -165,6 +151,11 @@
     </tbody>
     </table>
 </div>
+    </div>
+</div>
+
+    
+    </div>
   <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.22/css/jquery.dataTables.css">
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <!-- script de jquery para que funcione el buscador de nombre-->
@@ -205,41 +196,53 @@ $(document).ready( function () {
 <div class="modal fade" id="configurar">
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
-            <div class="modal-header" style="background-color:#293d3d; color:white;  height:100px; ">
-              <h4 class="modal-title" id="myModalLabel">Agregar Alerta Predefinida</h4>      
+            <div class="modal-header" style="background-color:#276678; color:white; ">
+            <h4 class="modal-title" id="myModalLabel">Agregar Alerta Predefinida</h4> 
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">x</button>
             </div>
 
             <div class="modal-body">
                 <form method="POST"  action="{{route('alertaspredeterminada.guardar',['id'=>$pacientes->id])}}">
                 @csrf
-                <textarea cols="60" required id="areadetexto" name="alertapredeterminada" placeholder="Escriba la alerta que aparecera disponible para ser seleccionada en todos los pacientes"></textarea>
-                <hr>
-                <div>
-                <button type="submit" class="btn btn-primary" id="agregar" >Agregar </button>
+                <textarea cols="55" required id="areadetexto" name="alertapredeterminada" placeholder="Escriba la alerta que aparecera disponible para ser seleccionada en todos los pacientes"></textarea>
+              
+                <div class="modal-footer">
+                <button type="submit" class="btn" style="background-color:#1687A7;color:white;" id="agregar" >Agregar </button>
                 </div>
                 </form>
 
-            <div>
+            <div >
+                <table class="table"><thead>
+                    <tr>
+                        <td>Alerta</td>
+                        <td>Accion</td>
+                    </tr></thead>
+                    <tbody>
+                   
+               
                 <?php 
                 $alertapredefinida= App\Alertapredeterminada::All();?>
                 @forelse($alertapredefinida as $alertapredefinida2)
-                <label> {{$alertapredefinida2->alertapredeterminada}}</label>
+                 <tr>
+                 <td>
+               <label style="margin:1em;"> {{$alertapredefinida2->alertapredeterminada}}</label></td>
+             
                 <!-- seccion de eliminar alertas predefinidas -->
                 @can('isAdmin')
-                <td>
+                <td> 
                     <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#modalalertapredefinida2-{{$alertapredefinida2->id}}"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                     <path fill-rule="evenodd" d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5a.5.5 0 0 0-1 0v7a.5.5 0 0 0 1 0v-7z"/>
                     </svg>
-                    Eliminar
+                    
                     </button>
                     
-                </td>
-                @endcan
+            
+                
                     <!-- Modal de eliminar -->
                     <div class="modal fade" id="modalalertapredefinida2-{{$alertapredefinida2->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                    <div class="modal-dialog" role="document">
+                    <div class="modal-dialog  modal-dialog-centered" role="document">
                     <div class="modal-content">
-                    <div class="modal-header" style="background-color:#a3c2c2; color:white;  height:80px; ">
+                    <div class="modal-header" style="background-color:#276678; color:white;  height:80px; ">
                     <h5 class="modal-title" id="exampleModalLabel"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                     <path fill-rule="evenodd" d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5a.5.5 0 0 0-1 0v7a.5.5 0 0 0 1 0v-7z"/>
                     </svg> Eliminar Alerta Predeterminada</h5>
@@ -248,7 +251,7 @@ $(document).ready( function () {
                     </button>
                     </div>
                     <div class="modal-body">
-                    ¿Desea realmente eliminar la alerta?
+                    ¿Desea realmente eliminar la alerta {{$alertapredefinida2->alertapredeterminada}}?
                     </div>
                     <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
@@ -263,12 +266,14 @@ $(document).ready( function () {
                     </div>
                     </div>
             <!-- fin de modal -->
-
+    </td></tr>@endcan
+                 @empty
+               <td colspan="2">No se ha creado ningula alerta predeterminada todavia</td> 
+                @endforelse
+                </tbody></table>
                 <br>
 
-                @empty
-                No se han creado todavia
-                @endforelse
+               
             </div>
             </div>
         </div> 
