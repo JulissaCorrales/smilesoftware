ba24dfc56b4387c3982985bea1021a65ba3c1bfa
junioranibalsalinas <junior.salinas@unah.hn>
2021-06-20 16:16:44 -0600

    corregir detalle de las vista de inventarios le faltaba una n a la palabra inventarios

--- resources/views/inventarios.blade.php
+++ resources/views/inventarios.blade.php
@@ -21,7 +21,7 @@
   <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"/>
   <path d="M7 5.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 1 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0zM7 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 0 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0z"/>
 </svg>  Inventarios de la Clínica </h4>
-                <p>En esta Sección se muestra los Iventarios Existentes en la actualidad y en seguridad, también se podra editar datos, crear un nuevos Inventarios, borrar los inventarios existentes y descargar el archivo el archivo con todos los Inventarios.</p>
+                <p>En esta Sección se muestra los Inventarios Existentes en la actualidad y en seguridad, también se podra editar datos, crear un nuevos Inventarios, borrar los inventarios existentes y descargar el archivo el archivo con todos los Inventarios.</p>
           </div>
           <!-- -->
 
