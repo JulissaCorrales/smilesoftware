8e1eab237a991f02b36164e12ba256812d722d6e
Fredal Antonio Vasquez <antoniohilson21@gmail.com>
2021-07-02 22:42:06 -0600

    las

--- resources/views/citaIndividual.blade.php
+++ resources/views/citaIndividual.blade.php
@@ -35,14 +35,14 @@
             <path d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2z"/>
             <path d="M1 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1z"/>
             </svg> Citas</h4>
-            <p>En esta sección se muestra las citas registrados y también se podrá agendar nuevas citas</p>
+            <p>En esta sección se muestra las citas registrados y también se podrá agendar las nuevas citas</p>
         
             @can('create',App\Cita::class)
             <button  type="button" class="btn btn-outline-info" id="darcita" style="background-color:#1687a7; color:white; "  data-toggle="modal" data-target="#create" width="50px" >  
             <svg width="15" height="15" viewBox="0 0 16 16" class="bi bi-calendar2-plus-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
             <path fill-rule="evenodd" d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM2 3.5c0-.276.244-.5.545-.5h10.91c.3 0 .545.224.545.5v1c0 .276-.244.5-.546.5H2.545C2.245 5 2 4.776 2 4.5v-1zm6.5 5a.5.5 0 0 0-1 0V10H6a.5.5 0 0 0 0 1h1.5v1.5a.5.5 0 0 0 1 0V11H10a.5.5 0 0 0 0-1H8.5V8.5z"/>
             </svg>
-            Agendar Cita</button>
+            Agendar Cita </button>
             @endcan
          
                             <!-- descargar -->
