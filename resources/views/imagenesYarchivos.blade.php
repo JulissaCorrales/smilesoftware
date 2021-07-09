30c9b329d925b864f5823c7c0af25b1acc70aad5
Fredal Antonio Vasquez <antoniohilson21@gmail.com>
2021-07-09 14:21:31 -0600

    cambio de titulo en pestaña de IMAGENES Y ARCHIVOS A IMAGENES CLINICAS DEL PACIENTE

--- resources/views/imagenesYarchivos.blade.php
+++ resources/views/imagenesYarchivos.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @section('titulo','Imagenes y Archivos')
+    @section('titulo','Imagenes Cinicas del paciente')
   
     <style>
 @import url(https://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700|Oswald:400,300,700);
