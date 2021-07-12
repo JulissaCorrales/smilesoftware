b9863aad99807f2730526c27e762ffcae6d1ad43
junioranibalsalinas <junior.salinas@unah.hn>
2021-07-11 19:50:37 -0600

    error 24 error de redaccion en la vista de especialidades

--- resources/views/Especialidades.blade.php
+++ resources/views/Especialidades.blade.php
@@ -40,7 +40,7 @@
   <path d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2z"/>
   <path d="M1 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1z"/>
 </svg> Directorio de Especialidades</h4>
- <p>En esta ventana  muestra las especialidades de los Odontólogo que se han registrado  en la clínica, <br> en esta misma se podra crear un nuevas Especialidades asi mismo como asignarle nuevas especialidades a los Odontólogos y eliminar las especialidades con las que ya no se cuentan dentro de la clínica.</p>
+ <p>En esta ventana  muestra las especialidades de los Odontólogo que se han registrado  en la clínica, <br> en esta misma se podran crear nuevas Especialidades así mismo como asignarles nuevas especialidades a los Odontólogos y eliminar las especialidades con las que ya no se cuentan dentro de la clínica.</p>
 
   <!--Menu desplegable  -->
 
