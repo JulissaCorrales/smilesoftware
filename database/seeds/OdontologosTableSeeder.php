023196c05e2478d84c7c37a69bc46bc9c3fd0897
allison0899 <allison@unah.hn>
2021-07-13 19:11:27 -0600

    cambio

--- database/seeds/OdontologosTableSeeder.php
+++ database/seeds/OdontologosTableSeeder.php
@@ -56,25 +56,45 @@ class OdontologosTableSeeder extends Seeder
         $dias->save();
 
         $especialidad = new Especialidad();
-        $especialidad->Especialidad="Endodoncia";   
+        $especialidad->Especialidad="Endodoncia"; 
+       $especialidad->Descripcion="La endodoncia es un tratamiento dental conocido comúnmente para “matar el nervio”. Consiste en eliminar la parte profunda del diente cuando se encuentra lesionado o infectado.";
         $especialidad->save();
 
         $especialidad = new Especialidad();
         $especialidad->Especialidad="Ortodoncia";
+        $especialidad->Descripcion="La ortodoncia es una especialidad de la odontología que se encarga de todo el estudio, prevención, diagnóstico y tratamiento de las anomalías de forma, posición, relación y función de las estructuras dentomaxilofaciales.";
         $especialidad->save();
 
  $especialidad = new Especialidad();
         $especialidad->Especialidad="General";
+        $especialidad->Descripcion="La medicina general brinda atención médica y es imprescindible para la prevención, detección, tratamiento y seguimiento de las enfermedades, es responsable del paciente, para decidir su derivación a los especialistas cuando alguna patología se descompense";
         $especialidad->save();
 
  $especialidad = new Especialidad();
         $especialidad->Especialidad="Prostodoncia";
+        $especialidad->Descripcion="Prostodoncia es la especialidad dental dedicada a la restauración y reemplazo de dientes ausentes o dañados.";
         $especialidad->save();
 
  $especialidad = new Especialidad();
-        $especialidad->Especialidad="Patologías Bucales y Maxilofaciales";
+        $especialidad->Especialidad="Periodoncia";
+        $especialidad->Descripcion="Estudia la prevención, diagnóstico y tratamiento de las enfermedades y condiciones que afectan a los tejidos que dan soporte a los órganos dentarios, para el mantenimiento de la salud, función y estética de los dientes, sus tejidos adyacentes.";
         $especialidad->save();
 
+       
+$especialidad = new Especialidad();
+        $especialidad->Especialidad="Odontopediatría";
+        $especialidad->Descripcion="La Odontopediatría  atiende y trata las distintas enfermedades bucodentales desde la infancia más temprana hasta finalizar el crecimiento. Por tanto, el odontopediatra se encargará de explorar la cavidad oral del menor y detectar posibles anomalías";
+        $especialidad->save();
+
+$especialidad = new Especialidad();
+        $especialidad->Especialidad="Radiología maxilofacial y oral";
+        $especialidad->Descripcion="La radiología oral y maxilofacial, también conocida como radiología dental y maxilofacial, es la especialidad de la odontología que se ocupa del desempeño e interpretación de las imágenes de diagnóstico.";
+        $especialidad->save();
+
+$especialidad = new Especialidad();
+        $especialidad->Especialidad="Cirugía maxilofacial y oral.";
+        $especialidad->Descripcion="Es una especialidad quirúrgica que incluye el diagnóstico, cirugía y tratamientos relacionados de un gran espectro de enfermedades, heridas y aspectos estéticos de la boca, dientes, cara, cabeza y cuello.";
+        $especialidad->save();
 
 
         //seeder para los dias de Semana
