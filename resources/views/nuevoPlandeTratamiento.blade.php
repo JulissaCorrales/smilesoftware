0a1a7a718ebb408bde906d1ea43b9d0f0d15514f
allison0899 <allison@unah.hn>
2021-07-14 00:37:47 -0600

    nuevoplan de tratamiento

--- resources/views/nuevoPlandeTratamiento.blade.php
+++ resources/views/nuevoPlandeTratamiento.blade.php
@@ -5,22 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nuevo Plan de Tratamiento</title>
-    <style>
-    #todo{
-    margin: 5rem;
-    padding: 2rem;
-    border: 5px solid gray;
-    background-color: #b3cccc;
-position:absolute;
-left: 400px;
-width: 800px;
-top: 100px;
-    }
-    h2{
-      text-align:center;
-    }
-  
-    </style>
+    
     
 </head>
 @section('cuerpo')
@@ -41,73 +26,87 @@ top: 100px;
             {{session('mensaje')}}
         </div>
     @endif
-    <div id="todo">
-    <h2>Creación de Nuevo Tratamiento</h2>
-    <h4>Paciente # {{$pacientes->id}}</h4>
 
+<div class="card mb-3">
+    <div class="card-header">
+    <h3>Creación de Plan de Tratamiento</h3>
+    <h4>Paciente: {{$pacientes->nombres}} {{$pacientes->apellidos}}</h4>
+    </div>
 
      <!-- Esta parte del codigo es para poder ir a traer informacion de la base de datos -->
      <?php
         $mysqli= new mysqli ('127.0.0.1','root','','smilesoftware');
         $mysqli->set_charset("utf8");
       ?>
-
+<div class="card-body">
         <form method="post" action="">
+
                       @csrf
-                    <div class="form-group">
+                    <div class="row" >
+                      <div class="col-md-10" >
                     <label for="nombreTratamiento">Nombre del Tratamiento:</label>
                     <select required name="tratamiento_id" class="form-control">
-                    <label for="nombreTratamiento">Nombre del Tratamiento:</label>
                         <option value="" disabled selected>Seleccione el Nombre del Tratamiento</option>
                         
                                 @forelse ($tratamientos as $tag) 
                                 <option value={{$tag->id}} >{{$tag->categoria}} </option>
                                 @empty
-                                <option disabled selected> ¡¡La clinica todavia no tiene un tratamiento.!!Cree uno por favor</option>
+                                <option disabled selected> ¡¡La clínica todavia no tiene un tratamiento.!!Cree uno por favor</option>
                                
                                 @endforelse 
 
-
-                     
                     </select>
-                          <!-- <label for="nombreTratamiento">Nombre del Tratamiento:</label>
-                          <input type="text" class="form-control-file" name="nombreTratamiento" id="nombreTratamiento" placeholder="ingresar nombre del tratamiento"> -->
-                    </div>
+                     
+                    </div></div>
 
-                      <div class="form-group">
+                      <div class="row" >
+                      <div class="col-md-10" >
                             <label for="estado" class="control-label">Estado:</label>
                 <select required name="estado" id="estado" class="form-control">
                 <option value="" disabled selected>Seleccione el estado del tratamiento</option>
                 <option >Activo</option>
                 <option >Finalizado</option>
                 </select>
-                
+                </div></div>
 
   <!-- paciente -->
-        <!--cita -->
-  <label for="state_id" class="control-label">Cita:</label>
-        <select required name="cita_id" class="form-control">
-        <option value="" disabled selected>Seleccione la cita </option>
-        @forelse ($pacientes->citas as $tag) 
-        <option value={{$tag->id}} >{{$tag->stard}} Con el Doctor: {{$tag->odontologo->nombres}} {{$tag->odontologo->apellidos}}</option>
-        @empty
-        <option disabled selected>¡¡Todavia no tiene una cita!! Asignele una por favor.</option>
-        todavia no tiene una cita
-        @endforelse
-        </select>
-
+        <!--cita --><div class="row" >
+                      <div class="col-md-8" >
+                        <label for="state_id" class="control-label">Cita:</label>
+                                <select required name="cita_id" class="form-control">
+                                <option value="" disabled selected>Seleccione la cita </option>
+                                @forelse ($pacientes->citas as $tag) 
+                                <option value={{$tag->id}} >{{$tag->stard}} Con el Doctor: {{$tag->odontologo->nombres}} {{$tag->odontologo->apellidos}}</option>
+                                @empty
+                                <option disabled selected>¡¡Todavia no tiene una cita!! Asignele una por favor.</option>
+                                todavia no tiene una cita
+                                @endforelse
+                                </select>
+                        </div>
+                        <div class="col-md-2" >
+                        @can('create',App\Cita::class)
+                                    <button  type="button" style="margin:3em;"class="btn btn-outline-info" id="darcita" style="background-color:#1687a7; color:white; "  data-toggle="modal" data-target="#create" >  
+                                    <svg width="15" height="15" viewBox="0 0 16 16" class="bi bi-calendar2-plus-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM2 3.5c0-.276.244-.5.545-.5h10.91c.3 0 .545.224.545.5v1c0 .276-.244.5-.546.5H2.545C2.245 5 2 4.776 2 4.5v-1zm6.5 5a.5.5 0 0 0-1 0V10H6a.5.5 0 0 0 0 1h1.5v1.5a.5.5 0 0 0 1 0V11H10a.5.5 0 0 0 0-1H8.5V8.5z"/>
+                                    </svg>
+                                    Agendar Cita </button>
+                                    @endcan
+                        </div>
         
     
-    
+    <div class="modal-footer">
     <br>
-    <button style="background-color:purple"type="button" onclick="location.href='/pantallainicio/vista/paciente/{{ $pacientes->id}}/plandetratamiento'"class="btn btn-secondary" data-dismiss="modal">Atrás</button>
-        <input type="reset" class="btn btn-danger">
-         <button type="submit" class="btn btn-primary" >Guardar Tratamiento</button>
+    <button type="button" onclick="location.href='/pantallainicio/vista/paciente/{{ $pacientes->id}}/plandetratamiento'"class="btn btn-outline-secondary" data-dismiss="modal">Atrás</button>
+        <input type="reset" class="btn btn-outline-danger">
+         <button type="submit" class="btn btn-outline-primary" >Guardar Tratamiento</button>
        
-        
+        </div>
      </form>
 
 </div>
+</div>
 </body>
-@endsection
+
 </html>
+@include('darcita')
+@endsection
