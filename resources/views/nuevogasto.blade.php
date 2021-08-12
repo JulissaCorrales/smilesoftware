2c2f545cfc1b1119d4d0f469396cde0ea675a940
allison0899 <allison@unah.hn>
2021-08-12 10:15:05 -0600

    gastos

--- resources/views/nuevogasto.blade.php
+++ resources/views/nuevogasto.blade.php
@@ -68,7 +68,7 @@
                     <!-- Monto-->
                     <div class="form-group" id="div3">
                     <label for="monto" class="control-label">Monto:</label>
-                    <input  required type="number" min="1" pattern="^[0-9]+" class="form-control custom-select" name="monto" id="monto" placeholder="Ingrese el monto del gasto" formControlName="precio_min">
+                    <input  required type="number" min="1" pattern="^[0-9]+" class="form-control custom-select" name="monto" id="monto" placeholder="Ingrese el monto del gasto" formControlName="precio_min" oninput="this.value = Math.max(this.value, 1)">
                     </div>
                  
                     <!-- Fecha de la factura-->
